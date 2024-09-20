c3c266d44541a66f3f86d55721c7c43a73b5c767
ky9888 <135335702+ky9888@users.noreply.github.com>
2024-09-20 16:06:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,30 +13,12 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || process.env.URL_API;
 const URI_DB = process.env.URI_DB;
-connect(URI_DB);
-
-
-// app.use(
-//   cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 })
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-
-// const allowedOrigins = [
-//   'http://localhost:3000',
-//   'https://be-e-commerce-3ayq.onrender.com'
-// ];
-
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   }
-// }));
+connect(URI_DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  connectTimeoutMS: 30000,  
+  socketTimeoutMS: 60000  
+});
 
 app.use(cors({
   origin: process.env.URL_CLIENT 
