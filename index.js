e849a78722bdea95a891d90620380a616b04847e
ky9888 <135335702+ky9888@users.noreply.github.com>
2024-09-22 12:44:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const PORT = process.env.PORT || process.env.URL_API;
 const URI_DB = process.env.URI_DB;
 connect(URI_DB,{
-  connectTimeoutMS: 15000,  
+  connectTimeoutMS: 30000,  
 });
 
 app.use(cors({
