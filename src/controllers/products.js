807357cee507dd59157e95ab196deb12929f5b33
ky <nvk250698@gmail.com>
2025-01-05 12:41:51 +0700

    update

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,20 +1,6 @@
 import products from "../models/product.js";
 import { remove as removeDiacritics } from "diacritics";
 
-export const createProducts = async (req, res) => {
-  const newProduct = new products(req.body);
-  try {
-    const saveProduct = await newProduct.save();
-    res.status(200).json({
-      message: "tạo sản phẩm thành công",
-      data: saveProduct,
-    });
-  } catch (error) {
-    res.status(400).json({
-      message: "tạo sản phẩm thất bại",
-    });
-  }
-};
 
 const diacriticMap = {
   "dong ho": "đồng hồ",
@@ -59,66 +45,37 @@ export const getAllProduct = async (req, res) => {
   }
 };
 
-export const getDetailProduct = async (req, res) => {
-  let productName = req.params.name;
 
+
+export const getDetailProduct=async(req,res)=>{
   try {
-    const productDetail = await products.findOne({ name: productName });
-    return res.status(200).json({
-      message: "tìm  sản phẩm thành công",
-      data: productDetail,
-      date: productName,
+      const productDetail=await products.findById(req.params.id)
+      return res.status(200).json({
+          message:"tìm  sản phẩm thành công",
+          data:productDetail
+      }) 
+  } catch (error) {
+      res.status(500).send(error.message);
+      
+  }
+}
+
+export const createProducts = async (req, res) => {
+  const newProduct = new products(req.body);
+  try {
+    const saveProduct = await newProduct.save();
+    res.status(200).json({
+      message: "tạo sản phẩm thành công",
+      data: saveProduct,
     });
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(400).json({
+      message: "tạo sản phẩm thất bại",
+      
+    });
   }
 };
 
-export const getHighestPrice = async (req, res) => {
-  try {
-    const { name, ...query } = req.query;
-
-    let productsData;
-
-    productsData = await products.find(query);
-
-    return res.status(200).json({
-      message: name ? "Tìm sản phẩm thành công" : "Lấy sản phẩm thành công",
-      data: productsData,
-    });
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-};
-export const getTopicProducts = async (req, res) => {
-  let productTopic = req.params.topic;
-
-  try {
-    const productDetailTopic = await products.find({ topic: productTopic });
-    return res.status(200).json({
-      message: "tìm  sản phẩm thành công",
-      data: productDetailTopic,
-      date: productTopic,
-    });
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-};
-export const getTypeProducts = async (req, res) => {
-  let productType = req.params.type;
-  console.log(productType);
-
-  try {
-    const productDetailType = await products.find({ type: productType });
-    return res.status(200).json({
-      message: "tìm  sản phẩm thành công",
-      data: productDetailType,
-      date: productType,
-    });
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-};
 export const updateProduct = async (req, res) => {
   try {
     const updateProducts = await products.findByIdAndUpdate(
